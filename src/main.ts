bc31d7b4c3a99f0eff3de65cac871e7f547aacc3
Dhouibi Mohamed Aziz <108880033+3zzouz@users.noreply.github.com>
2023-09-11 10:51:40 +0100

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -6,44 +6,44 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 //connexion au serveur
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const config = new DocumentBuilder()
-    .setTitle('E-Learning Platform')
-    .setDescription('Formation NestJs')
-    .addTag('users')
-    .addTag('categories')
-    .addTag('courses')
-    .addTag('sections')
-    .addTag('reviews')
-    .addTag('quizs')
-    .addTag('quizcours')
-    .addTag('students')
-    .addTag('trainers')
-    .addTag('auth')
-    .addBearerAuth(
-      {
-        description: 'Please Enter Token in the following format:Bearer<JWT>',
-        name: 'Authorization',
-        scheme: 'Bearer',
-        type: 'http',
-        in: 'Header',
-      },
-      'access-token',
-    )
-    .addBearerAuth(
-      {
-        description: 'Please Enter Token in the following format:Bearer<JWT>',
-        name: 'Authorization',
-        scheme: 'Bearer',
-        type: 'http',
-        in: 'Header',
-      },
-      'refresh-token',
-    )
-    .build();
+  const port = process.env.PORT || 3000
+ // const config = new DocumentBuilder()
+ // //  .setTitle('E-Learning Platform')
+  //  .setDescription('Formation NestJs')
+  //  .addTag('users')
+  //  .addTag('categories')
+  //  .addTag('courses')
+  //  .addTag('sections')
+  //  .addTag('reviews')
+  //  .addTag('quizs')
+  //  .addTag('quizcours')
+  //  .addTag('students')
+  //  .addTag('trainers')
+  //  .addTag('auth')
+  //  .addBearerAuth(
+    //  {
+     //   description: 'Please Enter Token in the following format:Bearer<JWT>',
+       // name: 'Authorization',
+       // scheme: 'Bearer',
+       // type: 'http',
+      ///  in: 'Header',
+   //   },
+    //  'access-token',
+   // )
+  //  .addBearerAuth(
+    //  {
+     //   description: 'Please Enter Token in the following format:Bearer<JWT>',
+     //   name: 'Authorization',
+     //   scheme: 'Bearer',
+     //   type: 'http',
+      //  in: 'Header',
+    //  },
+   //   'refresh-token',
+  //  )
+  ///  .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(5000);
+  //const document = SwaggerModule.createDocument(app, config);
+  //SwaggerModule.setup('api', app, document);
+  await app.listen(port);
 }
 bootstrap();
